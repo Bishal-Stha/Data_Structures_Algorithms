c39b0145d984b325f7b43ecc7f3b831a7faaf87c
Bishal-Stha <programmerbishal@gmail.com>
2026-01-26 20:19:16 +0545

    I learnt and programmatically implemented Tower of Hanoi.

---
+++ Recursion/TOH.md
@@ -0,0 +1,5 @@
+
+![Tower of Hanoi](image.png)
+---
+
+Tower of Hanoi is classical application of Recursion. It can only be solved using recursion, it can not be solved using iterative methods.
--- Recursion/towerOfHanoi.c
+++ Recursion/towerOfHanoi.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
-int TOH(int disks, char Source, char Auxilliary, char Destination){
-    if(disks==1)
+void TOH(int disks, char Source, char Auxilliary, char Destination){
+    if(disks>0){
+    TOH(disks-1,Source, Destination, Auxilliary);
     printf("disk %d is moved from %c to %c\n",disks, Source, Destination);
-    
+    TOH(disks-1,Auxilliary,Source,Destination);
+    }
+}
 
+int main(){
+    int n;
+    printf("enter no of disks: ");
+    scanf("%d",&n);
+    TOH(n,'A','B','C');
 }
